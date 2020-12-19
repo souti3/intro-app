4799b281742da91bdef9080f39c6ce361398715d
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2020-12-19 17:17:25 +0100

    [ADD] initial commit with spring framework

---
+++ src/test/java/ch/souti/introapp/IntroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.souti.introapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
